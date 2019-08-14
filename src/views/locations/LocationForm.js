42aba350a44e2e0388bb7922b6dcc6bb7b8dbda8
TrevorSeitz <trevinla@gmail.com>
2019-08-14 16:47:54 -0400

    photos saving to storage - download url saving to firestore with errors

--- src/views/locations/LocationForm.js
+++ src/views/locations/LocationForm.js
@@ -9,10 +9,10 @@ import React from "react";
 import Firebase from "firebase/app";
 import "firebase/storage";
 import shortid from "shortid";
-import { FilePond, registerPlugin } from "react-filepond";
+import { FilePond } from "react-filepond";
 import "filepond/dist/filepond.min.css";
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+// import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+// import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 // import { ImageUpload } from "../../actions/ImageUpload";
 import { FormRow, FormLabel, TextInput, TextArea } from "../../styles/forms";
 
@@ -20,7 +20,8 @@ export default class LocationForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      filePaths: []
+      fileNames: [],
+      urls: []
     };
   }
   onSubmit = event => {
@@ -32,8 +33,7 @@ export default class LocationForm extends React.Component {
       contactName,
       contactPhoneNumber,
       email,
-      description,
-      images
+      description
     } = event.target.elements;
     const values = {
       name: name.value,
@@ -43,7 +43,7 @@ export default class LocationForm extends React.Component {
       contactPhoneNumber: contactPhoneNumber.value,
       email: email.value,
       desciption: description.value,
-      images: this.state.filePaths
+      images: this.state.urls
     };
     console.log(values);
     this.props.onSubmit(values);
@@ -53,83 +53,119 @@ export default class LocationForm extends React.Component {
     // const [files, setFiles] = React.useState(defaultFiles);
     let files = [];
     let fileLocation;
-    let filePaths = [];
+    let fileNames = [];
     return (
-      <FilePond
-        files={files}
-        allowMultiple={true}
-        maxFiles={5}
-        server={{
-          process: (
-            _fieldName,
-            file,
-            _metadata,
-            load,
-            error,
-            progress,
-            _abort
-          ) => {
-            const id = shortid.generate();
-            const task = Firebase.storage()
-              .ref()
-              .child("images/" + id)
-              .put(file, {
-                contentType: "image/jpeg"
-              });
-
-            task.on(
-              Firebase.storage.TaskEvent.STATE_CHANGED,
-              snap => {
-                console.log("progress: %o", snap);
-                progress(true, snap.bytesTransferred, snap.totalBytes);
-                fileLocation = snap.ref.location.path_;
-                if (!filePaths.includes(fileLocation)) {
-                  filePaths.push(fileLocation);
-                }
-              },
-              err => {
-                console.log("error: %o", err);
-                error(err.message);
-              },
-              () => {
-                // let fileLocation = snap.ref.location.path_;
-                this.setState({ filePaths });
-                console.log("this is the list of file locations: ", filePaths);
-                console.log("DONE");
-                load(id);
-              }
-            );
-          },
-          load: (source, load, error, progress, abort) => {
-            progress(true, 0, 1024);
-            Firebase.storage()
-              .ref()
-              .child("images/" + source)
-              .getDownloadURL()
-              .then(url => {
-                let xhr = new XMLHttpRequest();
-                xhr.responseType = "blob";
-                xhr.onload = function(event) {
-                  let blob = xhr.response;
-                  console.log("loaded URL: %s", url);
-                  load(blob);
-                };
-                xhr.open("GET", url);
-                xhr.send();
-              })
-              .catch(err => {
-                error(err.message);
-                abort();
-              });
+      <fragment>
+        <FilePond
+          files={files}
+          labelIdle={
+            'Drag & Drop your files or <span class="filepond--label-action"> Browse </span>'
           }
-        }}
-      />
+          allowMultiple={true}
+          maxFiles={5}
+          server={{
+            process: (
+              _fieldName,
+              file,
+              _metadata,
+              load,
+              error,
+              progress,
+              _abort
+            ) => {
+              const id = shortid.generate();
+              const task = Firebase.storage()
+                .ref()
+                .child("images/" + id)
+                .put(file, {
+                  contentType: "image/jpeg"
+                });
+
+              task.on(
+                Firebase.storage.TaskEvent.STATE_CHANGED,
+                snap => {
+                  console.log("progress: %o", snap);
+                  progress(true, snap.bytesTransferred, snap.totalBytes);
+                  fileLocation = snap.ref.name;
+                  console.log(snap.ref.name);
+                  if (!fileNames.includes(fileLocation)) {
+                    fileNames.push(fileLocation);
+                  }
+                },
+                err => {
+                  console.log("error: %o", err);
+                  error(err.message);
+                },
+                () => {
+                  // let fileLocation = snap.ref.location.path_;
+                  this.setState({ fileNames });
+                  console.log(
+                    "this is the list of file locations: ",
+                    fileNames
+                  );
+                  console.log("DONE");
+                  load(id);
+                }
+              );
+            },
+            load: (source, load, error, progress, abort) => {
+              progress(true, 0, 1024);
+              Firebase.storage()
+                .ref()
+                .child("images/" + source)
+                .getDownloadURL()
+                .then(url => {
+                  let xhr = new XMLHttpRequest();
+                  xhr.responseType = "blob";
+                  xhr.onload = function(event) {
+                    let blob = xhr.response;
+                    console.log("loaded URL: %s", url);
+                    load(blob);
+                  };
+                  xhr.open("GET", url);
+                  xhr.send();
+                  console.log("hello!!!");
+                })
+                .then(url => {
+                  this.setState({
+                    fileNames: [...this.state.fileNames, url]
+                  });
+                })
+                .then(() =>
+                  console.log("does this catch url?", this.state.filePath)
+                )
+                .catch(err => {
+                  error(err.message);
+                  abort();
+                });
+            }
+          }}
+        />
+        <div>{this.imageDownload()}</div>
+      </fragment>
+    );
+  };
+
+  imageDownload = () => {
+    this.state.fileNames.map(file =>
+      Firebase.storage()
+        .ref()
+        .child("images/" + file)
+        .getDownloadURL()
+        .then(url => {
+          if (!this.state.urls.includes(url)) {
+            this.setState({
+              urls: [...this.state.urls, url]
+            });
+          }
+        })
+        .then(console.log("paths array:", this.state.urls))
     );
   };
 
   render() {
-    console.log("filePaths", this.state.filePaths);
-    // let filePaths = [];
+    console.log("fileNames", this.state.fileNames);
+    // let fileNames = [];
     return (
       <form onSubmit={this.onSubmit}>
         <FormRow>
